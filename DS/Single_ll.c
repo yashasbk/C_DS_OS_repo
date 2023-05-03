113f58458467b9621389202d6b36676cc7578a20
= <=>
2023-05-03 18:25:58 +0530

    Added reverse using data and link function, length and delete alternate functions

--- DS/Single_ll.c
+++ DS/Single_ll.c
@@ -30,28 +30,27 @@ void add_first(sll **head,int data)
 
 void add_last(sll **head,int data)
 {
-	sll *node = malloc(sizeof(sll));
+	sll *node = (sll*)malloc(sizeof(sll));
 	if(node == NULL)
 	{
 		printf("Memory allocation failed\n");
 	}
 	else
 	{
+        node->data = data;
+		node->next = NULL;
 		if(*head == NULL)
 		{
 			*head = node;
 		}
 		else
 		{
-			node->data = data;
-			node->next = NULL;
 			sll *temp = *head;
 			while(temp->next != NULL)
 			{
 				temp = temp->next;
 			}
 			temp->next = node;
-			temp = NULL;
         }
     }
 }
@@ -119,37 +118,122 @@ void display(sll **head)
     printf("\n");
 }
 
+int len(sll *head)
+{
+    int count = 0;
+    while(head != NULL)
+    {
+        head = head->next;
+        count++;
+    }
+    return count;
+}
+
+void reverse_ll(sll **head)
+{
+    sll *start,*end;
+    int temp = 0;
+    start=end=*head;
+    int i=0,j=len(*head)-1,k = 0;
+    while(i<j)
+    {
+        k = 0;
+        end = *head;
+        while(k < j)
+        {
+            end = end->next;
+            k++;
+        }
+        temp = start->data;
+        start->data = end->data;
+        end->data = temp;
+        start = start->next;
+        i++;
+        j--;
+    }
+}
+
+void reverse_link(sll **head)
+{
+    sll *prev,*next;
+    prev = NULL;
+    while(*head != NULL)
+    {
+        next = (*head)->next;
+        (*head)->next = prev;
+        prev = *head;
+        *head = next;
+    }
+    *head = prev;
+}
+
+void delete_alternate_node(sll **head)
+{
+    if(*head == NULL)
+    {
+        printf("Linked list is empty\n");
+    }
+    else
+    {
+        sll *prev,*next;
+        prev =  *head;
+        while(prev != NULL)
+        {
+            next = prev->next;
+            prev->next = next->next;
+            free(next);
+            prev = prev->next;
+        }
+    }
+}
+
 int main()
 {
-    char input = 0;
+    char input;
+    char c;
     int data = 0;
     sll *head = NULL;
     while(1)
     {
-        printf("What do you want to do:\n1)Add node at first\n2)Add node at last\n3)Delete node at first\n4)Delete node at last\n5)Display data in nodes\n6)Exit\n");
-        scanf("%hhd",&input);
+        printf("What do you want to do:\n1)Add node at first\n2)Add node at last\n3)Delete node at first\n"
+        "4)Delete node at last\n5)Display data in nodes\n6)Reverse LL\n"
+        "7)Revese link\n8)Delete Alternative node\n9)length of ll\nq)Exit\n");
+        input = getchar();
+        while ((c = getchar()) != '\n' && c != EOF);
         switch(input)
         {
-            case 1:
+            case '1':
                 printf("Enter the data\n");
                 scanf("%d",&data);
                 add_first(&head,data);
                 break;
-            case 2:
+            case '2':
                 printf("Enter the data\n");
                 scanf("%d",&data);            
                 add_last(&head, data);
                 break;
-            case 3:
+            case '3':
                 delete_first(&head);
                 break;
-            case 4:
+            case '4':
                 delete_last(&head);
                 break;
-            case 5:
+            case '5':
                 display(&head);
                 break;
-            case 6:
+            case '6':
+                reverse_ll(&head);
+                break;
+            case '7':
+                reverse_link(&head);
+                break;
+            case '8':
+                delete_alternate_node(&head);    
+                break;
+            case '9':
+                printf("Length is %d\n",len(head));
+                break;
+            case 'q':
                 return 0;
             default:
                 printf("Enter the option properly\n");
